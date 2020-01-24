c479903df0970e03f1e5026df5da750150e4f23b
Roo Williams <roo@roowilliams.com>
2020-01-23 17:03:16 -0800

    tweaks

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,11 @@ import React from "react"
 import styled from "styled-components"
 
 const Container = styled.footer`
-  padding: 1rem 0;
+  padding: 0.8rem 0;
   text-align: center;
-  background-color: rgb(10, 0, 13);
+  background-color: rgb(26, 27, 29);
   color: rgba(255, 255, 255, 0.4);
+  font-size: 0.8rem;
 `
 
 export default () => (
