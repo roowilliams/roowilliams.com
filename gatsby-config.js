7ca432b85132a5bb5e3b31768fc347e59d4b0673
unknown <roo@roowilliams.com>
2020-03-26 16:28:15 -0700

    update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,10 +73,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Roo Williams`,
+        short_name: `Roo Williams`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
